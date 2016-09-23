912884b112053a3112a17c5a16f117c31aef4195
Shahid Khaliq <shahid.khaliq@apimatic.io>
2016-09-23 20:20:50 +0500

    Updated version.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except(IOError, ImportError):
 
 setup(
     name='apimatic-cli',
-    version='1.0',
+    version='1.1',
     description='A command line interface for APIMatic.',
     long_description=long_description,
     author='Shahid Khaliq',
