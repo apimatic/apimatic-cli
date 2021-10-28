b25ebdfb7dc77e9f4b115ba2fbc58c57b044d428
Safeer Raees <safeerraeeszaidi@gmail.com>
2021-10-29 02:06:45 +0500

    Add validate via file and url Remaining validation messages

--- src/commands/api/validate.ts
+++ src/commands/api/validate.ts
@@ -1,13 +1,26 @@
+import * as fs from "fs";
+import {
+  ApiResponse,
+  APIValidationExternalApisController,
+  ApiValidationSummary,
+  Client,
+  FileWrapper
+} from "@apimatic/apimatic-sdk-for-js";
 import { flags, Command } from "@oclif/command";
 
-import { CLIClient } from "../../utils/client";
+import { SDKClient } from "../../client-utils/sdk-client";
+
+type GetValidationParams = {
+  file: string;
+  url: string;
+};
 
 export default class Validate extends Command {
   static description = "Validate your API specification to your supported formats";
 
   static examples = [
-    `$ apimatic api:validate --format="OpenApi3Json" --file="./specs/sample.json"
-File has been successfully validated into OpenApi3Json
+    `$ apimatic api:validate --file="./specs/sample.json"
+Specification file provided is valid
 `
   ];
 
@@ -19,10 +32,37 @@ File has been successfully validated into OpenApi3Json
     "auth-key": flags.string({ description: "Override current authKey by providing authKey in the command" })
   };
 
+  getValidation = async (flags: GetValidationParams, apiValidationController: APIValidationExternalApisController) => {
+    let validation: ApiResponse<ApiValidationSummary>;
+
+    if (flags.file) {
+      const file = new FileWrapper(fs.createReadStream(flags.file));
+      validation = await apiValidationController.validateAPIViaFile(file);
+
+      return validation.result;
+    } else if (flags.url) {
+      validation = await apiValidationController.validateAPIViaURL(flags.url);
+      return validation.result;
+    } else {
+      throw new Error("Please provide a specification file");
+    }
+  };
+
   async run() {
     const { flags } = this.parse(Validate);
-    const client = await CLIClient.getInstance().getClient(this.config.configDir);
 
-    this.log(`${client} ${flags}`);
+    try {
+      const overrideAuthKey = flags["auth-key"] ? flags["auth-key"] : null;
+      const client: Client = await SDKClient.getInstance().getClient(overrideAuthKey, this.config.configDir);
+
+      const apiValidationController: APIValidationExternalApisController = new APIValidationExternalApisController(
+        client
+      );
+
+      const { success }: ApiValidationSummary = await this.getValidation(flags, apiValidationController);
+      this.log(`${success ? "Specification file provided is valid" : "Specification is invalid"}`);
+    } catch (error: any) {
+      this.error(error);
+    }
   }
 }
