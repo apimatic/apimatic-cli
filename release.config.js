e9edf09086a9cedb6f157828a68ff5a7fe1311be
Safeer Raees <safeerraeeszaidi@gmail.com>
2021-11-26 12:56:09 +0500

    ci(release): add commitlint hook and modify release workflow

--- release.config.js
+++ release.config.js
@@ -1,25 +1,28 @@
-export const branches = [
-  {
-    name: "alpha",
-    prerelease: true
-  }
-];
-export const plugins = [
-  "@semantic-release/commit-analyzer",
-  "@semantic-release/release-notes-generator",
-  [
-    "@semantic-release/changelog",
+// eslint-disable-next-line no-undef
+module.exports = {
+  branches: [
     {
-      changelogFile: "CHANGELOG.md"
+      name: "alpha",
+      prerelease: true
     }
   ],
-  "@semantic-release/npm",
-  "@semantic-release/github",
-  [
-    "@semantic-release/git",
-    {
-      assets: ["CHANGELOG.md", "dist/**"],
-      message: "chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-    }
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md"
+      }
+    ],
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "dist/**"],
+        message: "chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ]
   ]
-];
+};
