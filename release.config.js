c314dd16a1c0cc1df1a98d35fc7d3b23cc8b592e
Safeer Raees <safeerraeeszaidi@gmail.com>
2021-11-26 12:25:38 +0500

    Add commit lint and release worflow with husky hook to check commits

---
+++ release.config.js
@@ -0,0 +1,25 @@
+export const branches = [
+  {
+    name: "alpha",
+    prerelease: true
+  }
+];
+export const plugins = [
+  "@semantic-release/commit-analyzer",
+  "@semantic-release/release-notes-generator",
+  [
+    "@semantic-release/changelog",
+    {
+      changelogFile: "CHANGELOG.md"
+    }
+  ],
+  "@semantic-release/npm",
+  "@semantic-release/github",
+  [
+    "@semantic-release/git",
+    {
+      assets: ["CHANGELOG.md", "dist/**"],
+      message: "chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }
+  ]
+];
