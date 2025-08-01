cc4ee2f07e7c29d9335497167ef7402a4ade23c3
Shield-Jaguar <mujahiddaudkhan@gmail.com>
2025-08-01 16:10:45 +0500

    refactor:  fix spaces

--- src/infrastructure/env-info.ts
+++ src/infrastructure/env-info.ts
@@ -4,7 +4,6 @@ import { dirname, join } from "path";
 import fs from "fs-extra";
 
 class EnvInfo {
-
   private static cachedCliVersion: string | null = null;
   private static cachedUserAgent: string | null = null;
 
@@ -18,7 +17,7 @@ class EnvInfo {
     return EnvInfo.cachedUserAgent;
   }
 
-  public  getCLIVersion(): string {
+  public getCLIVersion(): string {
     if (EnvInfo.cachedCliVersion) {
       return EnvInfo.cachedCliVersion;
     }
