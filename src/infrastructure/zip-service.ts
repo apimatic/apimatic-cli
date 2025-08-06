94a0271fd5bd1935e020724fceaafd3f6a3a99a3
Sohail <msohailshahbaz058@gmail.com>
2025-08-06 12:24:29 +0500

    Warning fix

--- src/infrastructure/zip-service.ts
+++ src/infrastructure/zip-service.ts
@@ -4,6 +4,9 @@ import extract from "extract-zip";
 import { DirectoryPath } from "../types/file/directoryPath.js";
 import { FilePath } from "../types/file/filePath.js";
 
+const MAX_FILES = 10000;
+const MAX_SIZE = 1000000000; // 1 GB
+
 export class ZipService {
   public async archive(sourceDir: DirectoryPath, outputZipPath: FilePath): Promise<void> {
     return new Promise((resolve, reject) => {
@@ -20,6 +23,33 @@ export class ZipService {
   }
 
   async unArchive(sourceFile: FilePath, destinationDirectory: DirectoryPath): Promise<void> {
-    await extract(sourceFile.toString(), { dir: destinationDirectory.toString() });
+    // await extract(sourceFile.toString(), { dir: destinationDirectory.toString() });
+    let fileCount = 0;
+    let totalSize = 0;
+
+    await extract(sourceFile.toString(), {
+      dir: destinationDirectory.toString(),
+      onEntry: function (entry) {
+        fileCount++;
+        if (fileCount > MAX_FILES) {
+          throw "Reached max. number of files";
+        }
+
+        // The uncompressedSize comes from the zip headers, so it might not be trustworthy.
+        // Alternatively, calculate the size from the readStream.
+        let entrySize = entry.uncompressedSize;
+        totalSize += entrySize;
+        if (totalSize > MAX_SIZE) {
+          throw "Reached max. size";
+        }
+
+        // if (entry.compressedSize > 0) {
+        //   let compressionRatio = entrySize / entry.compressedSize;
+        //   if (compressionRatio > THRESHOLD_RATIO) {
+        //     throw "Reached max. compression ratio";
+        //   }
+        // }
+      }
+    });
   }
 }
