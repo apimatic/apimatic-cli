cd5872aed327767f7197cdcc5ae23be8208723d2
Sohail <msohailshahbaz058@gmail.com>
2025-08-12 09:37:06 +0500

    SonarQube Warning

--- src/hooks/utils.ts
+++ src/hooks/utils.ts
@@ -3,7 +3,7 @@ import { blueBright, reset } from 'ansis';
 import { default as levenshtein } from 'fast-levenshtein';
 const getConfirmation = async (suggestion: string): Promise<boolean> => {
   if (!process.stdin.isTTY) return false;
-  const question = `${reset(`Did you mean ${blueBright(suggestion)}?`)} (Y/n) `;
+  const question = reset('Did you mean ' + blueBright(suggestion) + '?') + ' (Y/n) ';
   return await new Promise<boolean>((resolve) => {
     const rl = readline.createInterface({
       input: process.stdin,
