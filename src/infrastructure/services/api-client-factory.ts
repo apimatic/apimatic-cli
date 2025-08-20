1b2d2e8b4e2a975358dad00b450bc756c0cde649
Muhammad Sohail <62895181+sohail2721@users.noreply.github.com>
2025-08-20 16:27:17 +0500

    refactor: api:validate/transform (#179)

---
+++ src/infrastructure/services/api-client-factory.ts
@@ -0,0 +1,28 @@
+import { Client, Environment } from '@apimatic/sdk'; 
+import { envInfo } from '../env-info.js';
+
+export class ApiClientFactory {
+  private readonly TIMEOUT = 0;
+
+  public createApiClient = (authorizationHeader: string): Client => {
+    const baseConfig = {
+      customHeaderAuthenticationCredentials: {
+        Authorization: authorizationHeader
+      },
+      userAgent: envInfo.getUserAgent(),
+      timeout: this.TIMEOUT,
+    };
+
+    const baseUrl = envInfo.getBaseUrl();
+    
+    return new Client({
+      ...baseConfig,
+      environment: baseUrl ? Environment.Testing : Environment.Production,
+      ...(baseUrl && { customUrl: baseUrl })
+    });
+  };
+}
+
+export const apiClientFactory = new ApiClientFactory();
+
+export default ApiClientFactory;
