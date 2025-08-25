641e75db592f3106298c3730bc32c26428aff7b6
Shield-Jaguar <mujahiddaudkhan@gmail.com>
2025-08-25 13:55:56 +0500

    refactor: prompt for logout

--- src/commands/auth/status.ts
+++ src/commands/auth/status.ts
@@ -1,11 +1,13 @@
 import { Command } from "@oclif/core";
 
 import { SDKClient } from "../../client-utils/sdk-client.js";
+import { format } from "../../prompts/format.js";
 
 export default class Status extends Command {
   static description = "View the currently logged in user.";
 
-  static examples = [`apimatic auth status`];
+  private static cmdTxt = format.cmd('apimatic',  'auth' ,'status');
+  static examples = [Status.cmdTxt];
 
   async run() {
     try {
---
+++ src/prompts/auth/logout.ts
@@ -0,0 +1,9 @@
+import { log } from "@clack/prompts";
+
+
+export class LogoutPrompts {
+
+  public removeAuthInfo() {
+    log.info("Removing auth info");
+  }
+}
