08aad4ad608dc64530ec7e2f270d59f1fd40ce63
Shield-Jaguar <mujahiddaudkhan@gmail.com>
2025-08-26 10:55:30 +0500

    refactor: status

--- src/actions/auth/status.ts
+++ src/actions/auth/status.ts
@@ -1,5 +1,4 @@
 import { ActionResult } from "../action-result.js";
-import { SDKClient } from "../../client-utils/sdk-client.js";
 import { DirectoryPath } from "../../types/file/directoryPath.js";
 import { CommandMetadata } from "../../types/common/command-metadata.js";
 import { getAuthInfo } from "../../client-utils/auth-manager.js";
@@ -7,26 +6,24 @@ import { StatusPrompts } from "../../prompts/auth/status.js";
 import { ApiService } from "../../infrastructure/services/api-service.js";
 
 export class StatusAction {
-
   private readonly prompts = new StatusPrompts();
   private readonly apiService = new ApiService();
 
-  constructor(private readonly configDir: DirectoryPath, private readonly commandMetadata: CommandMetadata) {
-  }
+  constructor(private readonly configDir: DirectoryPath, private readonly commandMetadata: CommandMetadata) {}
 
   public async execute(): Promise<ActionResult> {
     const accountInfo = await getAuthInfo(this.configDir.toString());
     if (accountInfo === null) {
       return ActionResult.failed();
     }
-    const result =
-      await this.prompts.accountInfoSpinner(
-        this.apiService.getAccountInfo(this.configDir, this.commandMetadata.shell, accountInfo.authKey)
-      );
+    const result = await this.prompts.accountInfoSpinner(
+      this.apiService.getAccountInfo(this.configDir, this.commandMetadata.shell, accountInfo.authKey)
+    );
     if (result.isErr()) {
       this.prompts.invalidKeyProvided(result.error);
       return ActionResult.failed();
     }
+    this.prompts.showAccountInfo(result.value);
     return ActionResult.success();
   }
 }
--- src/prompts/auth/status.ts
+++ src/prompts/auth/status.ts
@@ -1,12 +1,11 @@
 import { Result } from "neverthrow";
 import { SubscriptionInfo } from "../../types/api/account.js";
 import { getErrorMessage, ServiceError } from "../../infrastructure/api-utils.js";
-import { withSpinner } from "../format.js";
+import { format, withSpinner } from "../format.js";
 import { log } from "@clack/prompts";
-
+import { Language } from "../../types/sdk/generate.js";
 
 export class StatusPrompts {
-
   public accountInfoSpinner(fn: Promise<Result<SubscriptionInfo, ServiceError>>) {
     return withSpinner(
       "Retrieving your subscription info",
@@ -20,5 +19,24 @@ export class StatusPrompts {
     const message =
       serviceError === ServiceError.NetworkError ? "Invalid API key provided." : getErrorMessage(serviceError);
     log.error(message);
+  }
+
+
+
+  public showAccountInfo(info: SubscriptionInfo) {
+    const activeLanguages = this.getActiveLanguages(info.allowedLanguages);
+    const message = [
+      `Email: ${format.var(info.Email)}`,
+      `Allowed Languages: ${format.var(activeLanguages.join(", "))}`,
+      `Api Copilot ID: ${format.var(info.ApiCopilotKeys.join(" "))}`
+    ].join("\n");
+    log.info(`Account Information:\n${message}`);
+  }
+
+
+
+
+  private getActiveLanguages(flags: number): string[] {
+    get
   }
 }
