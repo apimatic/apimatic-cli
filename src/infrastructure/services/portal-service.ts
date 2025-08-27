053edf3ab5c61fddd05ac4fc12541ed04b3c1aa5
Sohail <msohailshahbaz058@gmail.com>
2025-08-27 12:11:17 +0500

    made method readonly

--- src/infrastructure/services/portal-service.ts
+++ src/infrastructure/services/portal-service.ts
@@ -207,7 +207,7 @@ export class PortalService {
     }
   };
 
-  private languagePlatform: Record<Language, Platforms> = {
+  private readonly languagePlatform: Record<Language, Platforms> = {
     [Language.CSHARP]: Platforms.CsNetStandardLib,
     [Language.JAVA]: Platforms.JavaEclipseJreLib,
     [Language.PHP]: Platforms.PhpGenericLibV2,
