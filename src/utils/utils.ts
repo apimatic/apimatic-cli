e9890859c74d62f6fa031be10cd1c9bd6b220a3c
Ali Asghar <75574550+aliasghar98@users.noreply.github.com>
2025-09-01 11:34:42 +0500

    refactor: removed unused code

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,66 +1,10 @@
 import * as path from "path";
-import fs from "fs";
-import fsExtra from "fs-extra";
-import os from "os";
 import { Buffer } from "buffer";
-import archiver from "archiver";
 import stripTags from "striptags";
 import colors from "picocolors";
 
 import { loggers, ValidationMessages } from "../types/utils.js";
 
-export const createTempDirectory = async () => {
-  return fs.mkdtempSync(path.join(os.tmpdir(), "apimatic-cli-"));
-};
-
-export const deleteFile = async (filePath: string) => {
-  return await fsExtra.remove(filePath);
-};
-
-export const writeFileUsingReadableStream = (stream: NodeJS.ReadableStream, destinationPath: string) => {
-  return new Promise((resolve) => {
-    const writeStream = fs.createWriteStream(destinationPath);
-    stream.pipe(writeStream);
-    writeStream.on("close", () => {
-      resolve("Zipped");
-    });
-  });
-};
-
-/**
- * Packages local files into a ZIP archive
- *
- * @param {docsPortalFolderPath} path to portal directory.
- * @param {destinationZipPath} path to generated zip
- * return {string}
- */
-export const zipDirectory = async (sourcePath: string, destinationPath: string): Promise<string> => {
-  // Check if the directory exists for the user or not
-  await fsExtra.ensureDir(sourcePath);
-
-  const zipPath = path.join(destinationPath, ".target.zip");
-  const output = fs.createWriteStream(zipPath);
-  const archive = archiver("zip");
-
-  return new Promise((resolve, reject) => {
-    archive.on("error", (err: Error) => {
-      reject(new Error(err.message));
-    });
-
-    output.on("error", (err) => {
-      reject(new Error(err.message));
-    });
-    output.on("close", () => {
-      resolve(zipPath);
-    });
-
-    archive.pipe(output);
-
-    archive.directory(sourcePath, false);
-
-    archive.finalize().catch(reject);
-  });
-};
 
 export const replaceHTML = (string: string) => {
   return stripTags(string);
@@ -70,24 +14,6 @@ export const getFileNameFromPath = (filePath: string) => {
   return path.basename(filePath).split(".")[0];
 };
 
-export const printValidationMessages = (
-  { warnings, errors, messages }: ValidationMessages,
-  { log, warn, error }: loggers
-) => {
-  warnings = warnings || [];
-  messages = messages || [];
-  const singleError: string = errors.join("\n") || "";
-
-  messages.forEach((message) => {
-    log(`\nInfo: ${replaceHTML(message)}`);
-  });
-  warnings.forEach((warning) => {
-    warn(replaceHTML(warning));
-  });
-  if (errors.length > 0) {
-    error(replaceHTML(singleError));
-  }
-};
 
 export async function parseStreamBodyToJson(body: NodeJS.ReadableStream): Promise<any> {
   const chunks: Buffer[] = [];
