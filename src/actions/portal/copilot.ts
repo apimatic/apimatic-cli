1a0c7994282612c63a6fd2e8d06c4adc5ca56fb6
Shield-Jaguar <mujahiddaudkhan@gmail.com>
2025-09-18 18:12:20 +0500

    feat: reverted next steps

--- src/actions/portal/copilot.ts
+++ src/actions/portal/copilot.ts
@@ -35,10 +35,6 @@ export class CopilotAction {
     force: boolean,
     enable: boolean
   ): Promise<ActionResult> => {
-
-
-    this.prompts.copilotConfigured(enable, 'asdfadsfadsf');
-
     const buildContext = new BuildContext(buildDirectory);
 
     if (!(await buildContext.validate())) {
