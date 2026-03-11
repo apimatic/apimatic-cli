ceb7a0ddd2c197fa385a438e8c1a19b17b7c5b69
Asad Ali <14asadali@gmail.com>
2026-03-11 11:35:19 +0500

    nits: resolved TODO in versionedBuildContext

--- src/types/versioned-build-context.ts
+++ src/types/versioned-build-context.ts
@@ -8,32 +8,30 @@ type VersionedBuildValidateResult = { isValid: false; } | {
   versions: string[];
 };
 
-// TODO: remove BuildContext from this class
 export class VersionedBuildContext {
   private readonly fileService = new FileService();
-  private readonly buildContext: BuildContext;
   private readonly buildDirectory: DirectoryPath;
 
   constructor(buildDirectory: DirectoryPath) {
     this.buildDirectory = buildDirectory;
-    this.buildContext = new BuildContext(buildDirectory);
   }
 
   public async validate(): Promise<VersionedBuildValidateResult> {
-    if (!(await this.buildContext.validate())) {
+    const buildContext = new BuildContext(this.buildDirectory);
+    if (!(await buildContext.validate())) {
       return { isValid: false };
     }
-    const config = await this.buildContext.getBuildFileContents();
-    if (!config || !("generateVersionedPortal" in config)) {
+    const config = await buildContext.getBuildFileContents();
+    if (!("generateVersionedPortal" in config)) {
       return { isValid: false };
     }
-    const versionsPath = (config?.versionsPath as string) ?? "versioned_docs";
-    const versionsDir = this.buildDirectory.join(versionsPath);
-    if (!(await this.fileService.directoryExists(versionsDir))) {
+    const versionsPath = (config.versionsPath as string) ?? "versioned_docs";
+    const versionsDirectory = this.buildDirectory.join(versionsPath);
+    if (!(await this.fileService.directoryExists(versionsDirectory))) {
       return { isValid: false };
     }
-    const versionsDirs = await this.fileService.getSubDirectoriesPaths(versionsDir);
+    const versionsDirs = await this.fileService.getSubDirectoriesPaths(versionsDirectory);
     const versions = versionsDirs.map((dir) => dir.leafName());
-    return { isValid: true, versionsDirectory: versionsDir, versions: versions };
+    return { isValid: true, versionsDirectory, versions };
   }
 }
