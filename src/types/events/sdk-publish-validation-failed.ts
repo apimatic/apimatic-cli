8ef035dbf17c8ce68c567632609ac4cc48359d2f
Ali Asghar <75574550+aliasghar98@users.noreply.github.com>
2026-03-29 19:26:13 +0500

    fix: removed unnecessary tracking events and created single validation failure event

--- src/types/events/sdk-publish-validation-failed.ts
+++ src/types/events/sdk-publish-validation-failed.ts
@@ -1,7 +1,7 @@
 import { DomainEvent } from './domain-event.js';
 
-export class SdkPublishFailedEvent extends DomainEvent {
-  protected readonly eventName = SdkPublishFailedEvent.name;
+export class SdkPublishValidationFailedEvent extends DomainEvent {
+  protected readonly eventName = SdkPublishValidationFailedEvent.name;
 
   constructor(message: string, commandName: string, flags: Record<string, unknown>) {
     super(message, commandName, flags);
