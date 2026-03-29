47c8bece3172309e43a28f5aa52d3b5e1c668ca0
Ali Asghar <75574550+aliasghar98@users.noreply.github.com>
2026-03-29 19:24:49 +0500

    fix: create package configuration object when writing config

--- src/types/package-settings-context.ts
+++ src/types/package-settings-context.ts
@@ -1,4 +1,5 @@
 import { FileService } from '../infrastructure/file-service.js';
+import { toPackageConfiguration } from './publish-api/package-configuration.js';
 import { BaseConfigurationItem } from './publish-api/publishing-profile.js';
 import { Language } from './sdk/generate.js';
 import { DirectoryPath } from './file/directoryPath.js';
@@ -10,12 +11,17 @@ export class PackageSettingsContext {
 
   constructor(private readonly packageSettingsDirectory: DirectoryPath) {}
 
-  private settingsFileFor(language: Language): FilePath {
+  private getSettingsFilePath(language: Language): FilePath {
     return new FilePath(this.packageSettingsDirectory, new FileName(`${language}.json`));
   }
 
   public async writeConfiguration(configuration: BaseConfigurationItem, language: Language) {
+    const packageSettingsFilePath = this.getSettingsFilePath(language);
+    if (await this.fileService.fileExists(packageSettingsFilePath)) return;
     await this.fileService.createDirectoryIfNotExists(this.packageSettingsDirectory);
-    await this.fileService.writeContents(this.settingsFileFor(language), JSON.stringify(configuration, null, 2));
+    await this.fileService.writeContents(
+      packageSettingsFilePath,
+      JSON.stringify({ packageConfiguration: toPackageConfiguration(language, configuration) }, null, 2)
+    );
   }
 }
