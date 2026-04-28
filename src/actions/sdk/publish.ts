d420a283ac6faf09b4db37c34523e0062b3eb4f6
Ali Asghar <75574550+aliasghar98@users.noreply.github.com>
2026-04-28 10:10:51 +0500

    fix: pass package settings directory only when package settings applied

--- src/actions/sdk/publish.ts
+++ src/actions/sdk/publish.ts
@@ -39,11 +39,13 @@ export class SdkPublishAction {
   ): Promise<ActionResult<PublishingInfo>> => {
     return await withDirPath(async (tempDirectory) => {
       const packageConfiguration = publishingProfile.getPackageConfigurationForLanguage(language);
+      let packageSettingsTempDirectory: DirectoryPath | undefined;
       if (packageConfiguration !== null && packageConfiguration.isEnabled) {
         const packageSettingsConfiguration = PackageSettingsConfiguration.create(language, packageConfiguration);
         const packageSettingsDirectory = tempDirectory.join('package-settings');
         const packageSettingsContext = new PackageSettingsContext(packageSettingsDirectory);
         await packageSettingsContext.writeConfiguration(packageSettingsConfiguration, language);
+        packageSettingsTempDirectory = tempDirectory;
       }
 
       const sdkGenerateAction = new GenerateAction(this.configDir, this.commandMetadata);
@@ -57,7 +59,7 @@ export class SdkPublishAction {
         false,
         undefined,
         semVersion,
-        tempDirectory
+        packageSettingsTempDirectory
       );
       if (sdkGenerationResult.isFailed()) {
         return ActionResult.failed();
